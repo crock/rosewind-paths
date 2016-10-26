36719e167fefd1831fe82d0861ad27eee0cb18dc
Alex Crocker <croc122@gmail.com>
2016-10-26 13:55:01 -0400

    implemented login functionality without db connectivity

--- controller.php
+++ controller.php
@@ -6,4 +6,29 @@
 
         return str_replace('%TITLE%', $title, $head);
     }
+    
+    function authenticate_user($data) {
+		$default_user = "admin";
+		$default_pass = "test123";
+		$errors = [];
+		
+		if($data["username"] != $default_user) {
+			$errors += ["Error1" => "User Not Found"];
+		}
+		if($data["password"] != $default_pass) {
+			$errors += ["Error2" => "Incorrect Password"];
+		}
+		
+		if($errors == NULL) {
+			session_start();
+			$_SESSION["loggedIn"] = true;
+			header("Location: home.php");
+		} else {
+			header("Location: signin.php?error1={$errors['Error1']}?error2={$errors['Error2']}");
+		}
+    }
+    
+    if($_POST != NULL) {
+	    authenticate_user($_POST);
+    }
 ?>
--- signin.php
+++ signin.php
@@ -4,6 +4,29 @@
 	<?php echo rwp_head('Sign In'); ?>
 	<body>
 		<?php include("inc/header.php"); ?>
-		
+		<div class="container">
+			<form id="login-form" method="post" action="signin.php">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input class="form-control" type="text" id="username" name="username" placeholder="Username">
+					<? if($_GET["error1"]) { ?>
+						<span class="help-block">
+							<strong><? echo $_GET["error1"][1]; ?></strong>
+						</span>
+					<? } ?>
+				</div>
+				
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input class="form-control" type="password" id="password" name="password" placeholder="Password">
+					<? if($_GET["error2"]) { ?>
+						<span class="help-block">
+							<strong><? echo $_GET["error2"][1]; ?></strong>
+						</span>
+					<? } ?>
+				</div>
+				<button class="btn btn-success" type="submit">Sign In <span class="glyphicon glyphicon-send" aria-hidden="true"></span></button>
+			</form>
+		</div>
 	</body>
 </html>
