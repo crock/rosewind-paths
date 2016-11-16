0e12e7772aa715890c5d73449ecd4ebb38af1f85
Nani Jones <nkjones626@knights.ucf.edu>
2016-11-16 11:43:37 -0500

    Fixed CSS and gallery images, as well as the font in head.php and made changes to client.php

--- client.php
+++ client.php
@@ -14,7 +14,6 @@
 		<?php include("models/header.php"); ?>
 
 		<div class="container">
-			
 			<?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) { ?>
 				<h1> <?php echo $_SESSION['username']; ?> </h1>
 			<?php } else { ?>
@@ -22,7 +21,7 @@
 			<?php } ?>
 			<br/>
 			<div id="profile_intro">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam consequatur ea magnam incidunt vel, omnis, ipsa atque illo aliquam quis, at in praesentium excepturi enim maxime voluptatem nisi reiciendis ipsum doloremque distinctio. Reprehenderit id, repellat cumque nihil veritatis vel! Laborum, totam unde, sint minima inventore repellat pariatur perferendis dolores!
+				You haven't written anything on your profile yet.
 			</div>
 			<br/>
 			<h2>Shipping Information</h2>
@@ -54,9 +53,9 @@
 			<hr />
 			<h2>Gallery</h2>
 			<div class="row gallery">
-				<img class="col-sm-4 gallery_img" src="img/autumn-1804592_640.jpg" width="320" height="240"/>
-				<img class="col-sm-4 gallery_img" src="img/gibraltar-1351696_640.jpg" width="320" height="240"/>
-				<img class="col-sm-4 gallery_img" src="img/mountain-1806236_640.jpg" width="320" height="240"/>
+				<img class="col-md-4 gallery_img" src="img/autumn-1804592_640.jpg" />
+				<img class="col-md-4 gallery_img" src="img/gibraltar-1351696_640.jpg" />
+				<img class="col-md-4 gallery_img" src="img/mountain-1806236_640.jpg" />
 			</div>
 		</div><!-- end .container -->
 		<?php include("models/footer.php"); ?>
